277e981131f04184e51cbd850f9eb200f001e7f0
tohboeh5 <tohboeh5simsim7@gmail.com>
2025-11-29 14:35:42 +0000

    feat: Implement Milestone 2 - Live Indexer & Query Surface

--- ieapp-cli/src/ieapp/__init__.py
+++ ieapp-cli/src/ieapp/__init__.py
@@ -1,5 +1,9 @@
 """IEapp CLI package."""
 
+from .indexer import Indexer, query_index
 from .workspace import WorkspaceExistsError, create_workspace
 
-__all__ = ["WorkspaceExistsError", "create_workspace"]
+# Alias query_index to query for convenience
+query = query_index
+
+__all__ = ["WorkspaceExistsError", "create_workspace", "Indexer", "query"]
