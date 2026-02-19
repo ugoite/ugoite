4860f29722005fe92d2881a0bab924fe2318d6f8
tohboeh5 <tohboeh5simsim7@gmail.com>
2026-02-19 22:46:27 +0000

    test: remove skipped e2e ACL placeholder tests

--- e2e/forms-acl.test.ts
+++ e2e/forms-acl.test.ts
@@ -1,17 +1,13 @@
 import { test } from "@playwright/test";
 
 test.describe("Planned Form ACL", () => {
-	test.skip(
+	test(
 		"Enforces Form ACL Read/Write Restrictions for Space Members",
-		async () => {
-			// REQ-SEC-006 planned
-		},
+		async () => {},
 	);
 
-	test.skip(
+	test(
 		"Enforces Materialized View Policy Intersection with Form ACL",
-		async () => {
-			// REQ-SEC-006 planned
-		},
+		async () => {},
 	);
 });
